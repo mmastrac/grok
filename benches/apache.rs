0dc386a3c47ccbbf094bb1f4eca84595cbd71fd3
Nikolai Vazquez <hello@nikolaivazquez.com>
2025-06-10 14:38:17 -0400

    Migrate benchmarks to Divan

--- benches/apache.rs
+++ benches/apache.rs
@@ -1,126 +1,128 @@
-#![feature(test)]
-
-extern crate test;
+#![allow(clippy::incompatible_msrv)]
+// ^need 1.66 for `black_box`
 
 use grok::Grok;
-use test::Bencher;
 
-#[bench]
-fn bench_apache_log_match(b: &mut Bencher) {
+fn main() {
+    divan::main();
+}
+
+#[divan::bench]
+fn bench_apache_log_match(b: divan::Bencher) {
     let msg = r#"220.181.108.96 - - [13/Jun/2015:21:14:28 +0000] "GET /blog/geekery/xvfb-firefox.html HTTP/1.1" 200 10975 "-" "Mozilla/5.0 (compatible; Baiduspider/2.0; +http://www.baidu.com/search/spider.html)""#;
 
     let mut grok = Grok::default();
     let pattern = grok.compile(r#"%{IPORHOST:clientip} %{USER:ident} %{USER:auth} \[%{HTTPDATE:timestamp}\] "%{WORD:verb} %{DATA:request} HTTP/%{NUMBER:httpversion}" %{NUMBER:response} %{NUMBER:bytes} %{QS:referrer} %{QS:agent}"#, false)
         .expect("Error while compiling!");
 
-    b.iter(|| {
+    b.bench(|| {
         if let Some(found) = pattern.match_against(msg) {
-            test::black_box(&found);
+            divan::black_box(&found);
         }
     });
 }
 
-#[bench]
-fn bench_apache_log_no_match_start(b: &mut Bencher) {
+#[divan::bench]
+fn bench_apache_log_no_match_start(b: divan::Bencher) {
     let msg = r#"tash-scale11x/css/fonts/Roboto-Regular.ttf HTTP/1.1" 200 41820 "http://semicomplete.com/presentations/logs"#;
 
     let mut grok = Grok::default();
     let pattern = grok.compile(r#"%{IPORHOST:clientip} %{USER:ident} %{USER:auth} \[%{HTTPDATE:timestamp}\] "%{WORD:verb} %{DATA:request} HTTP/%{NUMBER:httpversion}" %{NUMBER:response} %{NUMBER:bytes} %{QS:referrer} %{QS:agent}"#, false)
         .expect("Error while compiling!");
 
-    b.iter(|| {
+    b.bench(|| {
         if let Some(found) = pattern.match_against(msg) {
-            test::black_box(&found);
+            divan::black_box(&found);
         }
     });
 }
 
-#[bench]
-fn bench_apache_log_no_match_middle(b: &mut Bencher) {
+#[divan::bench]
+fn bench_apache_log_no_match_middle(b: divan::Bencher) {
     let msg = r#"220.181.108.96 - - [13/Jun/2015:21:14:28 +0000] "111 /blog/geekery/xvfb-firefox.html HTTP/1.1" 200 10975 "-" "Mozilla/5.0 (compatible; Baiduspider/2.0; +http://www.baidu.com/search/spider.html)""#;
 
     let mut grok = Grok::default();
     let pattern = grok.compile(r#"%{IPORHOST:clientip} %{USER:ident} %{USER:auth} \[%{HTTPDATE:timestamp}\] "%{WORD:verb} %{DATA:request} HTTP/%{NUMBER:httpversion}" %{NUMBER:response} %{NUMBER:bytes} %{QS:referrer} %{QS:agent}"#, false)
         .expect("Error while compiling!");
 
-    b.iter(|| {
+    b.bench(|| {
         if let Some(found) = pattern.match_against(msg) {
-            test::black_box(&found);
+            divan::black_box(&found);
         }
     });
 }
 
-#[bench]
-fn bench_apache_log_no_match_end(b: &mut Bencher) {
+#[divan::bench]
+fn bench_apache_log_no_match_end(b: divan::Bencher) {
     let msg = r#"220.181.108.96 - - [13/Jun/2015:21:14:28 +0000] "GET /blog/geekery/xvfb-firefox.html HTTP/1.1" 200 10975 "-" 1"#;
 
     let mut grok = Grok::default();
     let pattern = grok.compile(r#"%{IPORHOST:clientip} %{USER:ident} %{USER:auth} \[%{HTTPDATE:timestamp}\] "%{WORD:verb} %{DATA:request} HTTP/%{NUMBER:httpversion}" %{NUMBER:response} %{NUMBER:bytes} %{QS:referrer} %{QS:agent}"#, false)
         .expect("Error while compiling!");
 
-    b.iter(|| {
+    b.bench(|| {
         if let Some(found) = pattern.match_against(msg) {
-            test::black_box(&found);
+            divan::black_box(&found);
         }
     });
 }
 
-#[bench]
-fn bench_apache_log_match_anchor(b: &mut Bencher) {
+#[divan::bench]
+fn bench_apache_log_match_anchor(b: divan::Bencher) {
     let msg = r#"220.181.108.96 - - [13/Jun/2015:21:14:28 +0000] "GET /blog/geekery/xvfb-firefox.html HTTP/1.1" 200 10975 "-" "Mozilla/5.0 (compatible; Baiduspider/2.0; +http://www.baidu.com/search/spider.html)""#;
 
     let mut grok = Grok::default();
     let pattern = grok.compile(r#"^%{IPORHOST:clientip} %{USER:ident} %{USER:auth} \[%{HTTPDATE:timestamp}\] "%{WORD:verb} %{DATA:request} HTTP/%{NUMBER:httpversion}" %{NUMBER:response} %{NUMBER:bytes} %{QS:referrer} %{QS:agent}$"#, false)
         .expect("Error while compiling!");
 
-    b.iter(|| {
+    b.bench(|| {
         if let Some(found) = pattern.match_against(msg) {
-            test::black_box(&found);
+            divan::black_box(&found);
         }
     });
 }
 
-#[bench]
-fn bench_apache_log_no_match_start_anchor(b: &mut Bencher) {
+#[divan::bench]
+fn bench_apache_log_no_match_start_anchor(b: divan::Bencher) {
     let msg = r#"tash-scale11x/css/fonts/Roboto-Regular.ttf HTTP/1.1" 200 41820 "http://semicomplete.com/presentations/logs"#;
 
     let mut grok = Grok::default();
     let pattern = grok.compile(r#"^%{IPORHOST:clientip} %{USER:ident} %{USER:auth} \[%{HTTPDATE:timestamp}\] "%{WORD:verb} %{DATA:request} HTTP/%{NUMBER:httpversion}" %{NUMBER:response} %{NUMBER:bytes} %{QS:referrer} %{QS:agent}$"#, false)
         .expect("Error while compiling!");
 
-    b.iter(|| {
+    b.bench(|| {
         if let Some(found) = pattern.match_against(msg) {
-            test::black_box(&found);
+            divan::black_box(&found);
         }
     });
 }
 
-#[bench]
-fn bench_apache_log_no_match_middle_anchor(b: &mut Bencher) {
+#[divan::bench]
+fn bench_apache_log_no_match_middle_anchor(b: divan::Bencher) {
     let msg = r#"220.181.108.96 - - [13/Jun/2015:21:14:28 +0000] "111 /blog/geekery/xvfb-firefox.html HTTP/1.1" 200 10975 "-" "Mozilla/5.0 (compatible; Baiduspider/2.0; +http://www.baidu.com/search/spider.html)""#;
 
     let mut grok = Grok::default();
     let pattern = grok.compile(r#"^%{IPORHOST:clientip} %{USER:ident} %{USER:auth} \[%{HTTPDATE:timestamp}\] "%{WORD:verb} %{DATA:request} HTTP/%{NUMBER:httpversion}" %{NUMBER:response} %{NUMBER:bytes} %{QS:referrer} %{QS:agent}$"#, false)
         .expect("Error while compiling!");
 
-    b.iter(|| {
+    b.bench(|| {
         if let Some(found) = pattern.match_against(msg) {
-            test::black_box(&found);
+            divan::black_box(&found);
         }
     });
 }
 
-#[bench]
-fn bench_apache_log_no_match_end_anchor(b: &mut Bencher) {
+#[divan::bench]
+fn bench_apache_log_no_match_end_anchor(b: divan::Bencher) {
     let msg = r#"220.181.108.96 - - [13/Jun/2015:21:14:28 +0000] "GET /blog/geekery/xvfb-firefox.html HTTP/1.1" 200 10975 "-" 1"#;
 
     let mut grok = Grok::default();
     let pattern = grok.compile(r#"^%{IPORHOST:clientip} %{USER:ident} %{USER:auth} \[%{HTTPDATE:timestamp}\] "%{WORD:verb} %{DATA:request} HTTP/%{NUMBER:httpversion}" %{NUMBER:response} %{NUMBER:bytes} %{QS:referrer} %{QS:agent}$"#, false)
         .expect("Error while compiling!");
 
-    b.iter(|| {
+    b.bench(|| {
         if let Some(found) = pattern.match_against(msg) {
-            test::black_box(&found);
+            divan::black_box(&found);
         }
     });
 }
--- benches/log.rs
+++ benches/log.rs
@@ -1,66 +1,68 @@
-#![feature(test)]
-
-extern crate test;
+#![allow(clippy::incompatible_msrv)]
+// ^need 1.66 for `black_box`
 
 use grok::Grok;
-use test::Bencher;
 
-#[bench]
-fn bench_log_match(b: &mut Bencher) {
+fn main() {
+    divan::main();
+}
+
+#[divan::bench]
+fn bench_log_match(b: divan::Bencher) {
     let msg = "2016-09-19T18:19:00 [8.8.8.8:prd] DEBUG this is an example log message";
 
     let mut grok = Grok::default();
     let pattern = grok.compile(r"%{TIMESTAMP_ISO8601:timestamp} \[%{IPV4:ip}:%{WORD:environment}\] %{LOGLEVEL:log_level} %{GREEDYDATA:message}", false)
         .expect("Error while compiling!");
 
-    b.iter(|| {
+    b.bench(|| {
         if let Some(found) = pattern.match_against(msg) {
-            test::black_box(&found);
+            divan::black_box(&found);
         }
     });
 }
 
-#[bench]
-fn bench_log_no_match(b: &mut Bencher) {
+#[divan::bench]
+fn bench_log_no_match(b: divan::Bencher) {
     let msg = "2016-09-19T18:19:00 [8.8.8.8:prd] DEBUG this is an example log message";
 
     let mut grok = Grok::default();
     let pattern = grok.compile(r"%{TIMESTAMP_ISO8601:timestamp} \[%{IPV4:ip};%{WORD:environment}\] %{LOGLEVEL:log_level} %{GREEDYDATA:message}", false)
         .expect("Error while compiling!");
 
-    b.iter(|| {
+    b.bench(|| {
         if let Some(found) = pattern.match_against(msg) {
-            test::black_box(&found);
+            divan::black_box(&found);
         }
     });
 }
 
-#[bench]
-fn bench_log_match_with_anchors(b: &mut Bencher) {
+#[divan::bench]
+fn bench_log_match_with_anchors(b: divan::Bencher) {
     let msg = "2016-09-19T18:19:00 [8.8.8.8:prd] DEBUG this is an example log message";
 
     let mut grok = Grok::default();
     let pattern = grok.compile(r"^%{TIMESTAMP_ISO8601:timestamp} \[%{IPV4:ip}:%{WORD:environment}\] %{LOGLEVEL:log_level} %{GREEDYDATA:message}$", false)
         .expect("Error while compiling!");
 
-    b.iter(|| {
+    b.bench(|| {
         if let Some(found) = pattern.match_against(msg) {
-            test::black_box(&found);
+            divan::black_box(&found);
         }
     });
 }
 
-#[bench]
-fn bench_log_no_match_with_anchors(b: &mut Bencher) {
+#[divan::bench]
+fn bench_log_no_match_with_anchors(b: divan::Bencher) {
     let msg = "2016-09-19T18:19:00 [8.8.8.8;prd] DEBUG this is an example log message";
 
     let mut grok = Grok::default();
     let pattern = grok.compile(r"^%{TIMESTAMP_ISO8601:timestamp} \[%{IPV4:ip}:%{WORD:environment}\] %{LOGLEVEL:log_level} %{GREEDYDATA:message}$", false)
         .expect("Error while compiling!");
 
-    b.iter(|| {
+    b.bench(|| {
         if let Some(found) = pattern.match_against(msg) {
-            test::black_box(&found);
+            divan::black_box(&found);
         }
     });
 }
